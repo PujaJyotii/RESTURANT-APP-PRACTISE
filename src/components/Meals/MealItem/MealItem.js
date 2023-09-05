3f3e39467b6cd810520aab173cc49aa59cc939e2
PujaJyotii <pujajyoti111>
2023-09-05 16:05:03 +0530

    change

---
+++ src/components/Meals/MealItem/MealItem.js
@@ -0,0 +1,17 @@
+import classes from './MealItem.module.css'
+
+const MealItem = props => 
+{
+    const price = `$${props.price.toFixed(2)}`;
+return (
+    <li className={classes.meal}>
+        <div>
+    <div>{props.name}</div>
+    <div className={classes.description}>{props.description}</div>
+    <div className={classes.price}>{price}</div>
+    </div>
+    </li>
+)
+}
+
+export default MealItem
