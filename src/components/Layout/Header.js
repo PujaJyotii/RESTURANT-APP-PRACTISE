d7bba63aefb0370e7c650696b878a95e49f1e3ce
PujaJyotii <pujajyoti111>
2023-09-06 12:29:43 +0530

    Changes

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -10,7 +10,7 @@ return (
     <Fragment   >
         <header className={classes.header}>
          <h1>Hey! Want Some Food</h1>
-         <HeaderCartButton />
+         <HeaderCartButton onClick={props.onShowCart}/>
         </header>
         <div className={classes['main-image']}>
          <img src={MealImage} alt="A table full of delicious food"/>
